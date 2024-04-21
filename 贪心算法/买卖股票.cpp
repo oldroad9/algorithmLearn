f39d9df66df71a823e0c39676c2ce67587dc18f4
oldroad-pc-own <1976328453@qq.com>
2024-04-21 20:36:19 +0800

    greedy algo 1.0

---
+++ 贪心算法/买卖股票.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int result = 0;
+        for (int i = 0; i < prices.size() - 2; i++) {
+            int profit = prices[i + 1] - prices[i];
+            result += max(profit, 0);
+        }
+        return result;
+    }
+};
